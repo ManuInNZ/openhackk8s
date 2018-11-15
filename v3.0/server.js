6b12befc3773fb9b8284a7e02aff745267a652e7
Andrew Trainor <andrew.trainor@fbu.com>
2018-11-15 15:27:58 +1300

    initial version

--- v3.0/server.js
+++ v3.0/server.js
@@ -1,74 +1,99 @@
 const express = require("express"),
-  app = express(),
-  promClient = require("prom-client"),
-  server = require("http").createServer(app),
-  exec = require("child_process").exec;
+    app = express(),
+    promClient = require("prom-client"),
+    server = require("http").createServer(app),
+    exec = require("child_process").exec,
+    rcon = require("rcon");
 
 let port = process.env.PORT || 3030;
 
-var currentPlayers =  0;
-var currentMaxPlayers = 0;
-var currentPopulation = new promClient.Counter({
+
+var currentPopulationGauge = new promClient.Gauge({
     name: "minecraft_population",
     help: "Population of the server."
-  });
+});
 
-const playerGauge = new promClient.Gauge({ name: 'metric_name', help: 'metric_help' });
-// TODO add labels for distinct values for current and max
+var playerGauge = new promClient.Gauge({
+    name: 'minecraft_users',
+    help: 'Number of users on the server'
+});
+
+var maxPlayerGauge = new promClient.Gauge({
+    name: 'minecraft_users_max',
+    help: 'Max number of users on the server'
+});
 
 function getPlayerMetrics() {
-  var result = new Array();
-  var conn = new rcon("localhost", 25575, "cheesesteakjimmys");
-  conn
-    .on("auth", function() {
-      console.log("Authed!");
-    })
-    .on("response", function(str) {
-      console.log("Got response: " + str);
-      result = str.split(" ");
-      currentPlayers = result[2];
-      currentMaxPlayers = result[6];
-      console.log("Current: " + currentPlayers);
-      /// playerGauge.set(currentPlayers); with the right label 
 
-    })
-    .on("end", function() {
-      console.log("Socket closed!");
-      process.exit();
+    var conn = new rcon("localhost", 25575, "cheesesteakjimmys", {
+        tcp: true,
+        challenge: false
     });
-  conn.connect();
-  conn.send("list");
+
+    conn.on('auth', function() {
+        console.log("Authed!");
+        this.send("list");
+
+    }).on('response', function(str) {
+        console.log("Got response: " + str);
+
+        var result = str.split(" ");
+
+        var currentPlayers = parseInt(result[2]);
+        var currentMaxPlayers = parseInt(result[6]);
+
+        console.log("Current: " + currentPlayers);
+        console.log("Max: " + currentMaxPlayers);
+
+        playerGauge.set(currentPlayers);
+
+        maxPlayerGauge.set(currentMaxPlayers);
+
+
+    }).on('end', function() {
+        console.log("Socket closed!");
+        process.exit();
+
+    });
+
+    conn.connect();
+
+
 }
 
-function getPopulation(params) {
-    population = exec('ls -l /metrics/world/playerdata | wc -l',  (error, stdout, stderr) => {
-        console.log(`Population ${stdout}`);
-        console.log(`${stderr}`);
-        if (error !== null) {
-            console.log(`exec error: ${error}`);
+function getPopulation() {
+    population = exec('ls -l /metrics/world/playerdata | wc -l', (error, stdout, stderr) => {
+            console.log(`Population ${stdout}`);
+            console.log(`${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+
+            var population = parseInt(stdout);
+
+            currentPopulationGauge
+                .set(population);
         }
-        currentPopulation.set(stdout);
-  }
-);
+    );
 }
 
 
-const hellosProvided = new promClient.Counter({
-  name: "hellos_provided",
-  help: "Number of Hello World! strings provided."
+app.get("/", function (req, res) {
+    hellosProvided.inc();
+    res.send("Hello World!");
 });
-app.get("/", function(req, res) {
-  hellosProvided.inc();
-  res.send("Hello World!");
-});
+
 app.get("/metrics", (req, res) => {
     getPlayerMetrics();
+    getPopulation();
 
-
-  res.set("Content-Type", promClient.register.contentType);
-  res.end(promClient.register.metrics());
+    res.set("Content-Type", promClient.register.contentType);
+    res.end(promClient.register.metrics());
 });
+
 server.listen(port);
 console.log("location server listening on port: " + port);
+
 promClient.collectDefaultMetrics();
+
 module.exports = server;
